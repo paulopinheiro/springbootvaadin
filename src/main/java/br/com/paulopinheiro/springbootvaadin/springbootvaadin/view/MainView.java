238bbdb37d91e31e25444ba331b89d7652f9ca10
Paulo Pinheiro <pinus.sc@gmail.com>
2025-05-08 19:47:23 -0300

    Implemented user interface

--- src/main/java/br/com/paulopinheiro/springbootvaadin/springbootvaadin/view/MainView.java
+++ src/main/java/br/com/paulopinheiro/springbootvaadin/springbootvaadin/view/MainView.java
@@ -1,64 +1,137 @@
 package br.com.paulopinheiro.springbootvaadin.springbootvaadin.view;
 
 import br.com.paulopinheiro.springbootvaadin.springbootvaadin.controller.WeatherService;
-import br.com.paulopinheiro.springbootvaadin.springbootvaadin.model.Coord;
 import br.com.paulopinheiro.springbootvaadin.springbootvaadin.model.Weather;
 import br.com.paulopinheiro.springbootvaadin.springbootvaadin.model.WeatherReport;
 import com.vaadin.flow.component.Text;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.html.Image;
+import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextArea;
+import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.Route;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 
 @Route("")
-public class MainView extends HorizontalLayout {
+public class MainView extends VerticalLayout {
+    private static final String ICON_URL = "http://openweathermap.org/img/w/";
+
     @Autowired
-    protected WeatherService weatherService;
+    private WeatherService weatherService;
+
+    private TextField cityTextField;
+    private Button queryButton;
+    private VerticalLayout dashboard = new VerticalLayout();
 
     public MainView() {
-        this.setWidth("200px");
-        String city = "navegantes";
+        // That shouldn't be necessary
+        // Springboot shoud inject weatherService with @Autowired
         if (weatherService == null) {
             System.out.println("Autowired failed!");
             weatherService = new WeatherService();
         }
 
-        executeQuery(city);
+        setUpLayout();
+        setUpForm();
+        setUpDashboard();
+    }
+
+    private void setUpLayout() {
+        setWidth("100%");
+        setMargin(true);
+        setSpacing(true);
+        setDefaultHorizontalComponentAlignment(Alignment.CENTER);
+    }
+
+    private void setUpForm() {
+        HorizontalLayout formLayout = new HorizontalLayout();
+        formLayout.setDefaultVerticalComponentAlignment(Alignment.CENTER);
+        formLayout.setSpacing(true);
+        formLayout.setMargin(true);
+
+        cityTextField = new TextField();
+        cityTextField.setLabel("City");
+        cityTextField.setTitle("City");
+        cityTextField.setWidth("80%");
+        add(cityTextField);
+
+        queryButton = new Button();
+        queryButton.addThemeVariants(ButtonVariant.LUMO_ICON);
+        queryButton.setText("Query");
+        queryButton.addClickListener(event -> {
+            executeQuery(this.cityTextField.getValue());
+        });
+        
+        add(queryButton);
+    }
+
+    private void setUpDashboard() {
+        dashboard.setDefaultHorizontalComponentAlignment(Alignment.CENTER);
+        dashboard.setWidthFull();
+        add(dashboard);
     }
 
     private void executeQuery(String city) {
         WeatherReport weatherReport = weatherService.getWeatherReport(city);
+        dashboard.removeAll();
 
         if (weatherReport!=null) {
-            add(new Text("Weather report for " + city));
-            showWeatherReport(weatherReport);
+            updateDashboard(weatherReport);
         }
         else {
-            add(new Text("No weather report found for " + city));
+           dashboard.add(new Text("No weather report found for " + city));
         }
     }
 
-    private void showWeatherReport(WeatherReport weatherReport) {
-        showWeatherList(weatherReport.getWeatherList());
+    private void updateDashboard(WeatherReport weatherReport) {
+        Span title = new Span("Weather report for " + weatherReport.getName() +
+                              " - " + weatherReport.getSys().getCountry());
+        title.getStyle().set("color", "blue");
+        title.getStyle().set("font-family", "Arial");
+        title.getStyle().set("font-size", "48px");
+        dashboard.add(title);
+
+        dashboard.add(getWeatherListLayout(weatherReport.getWeatherList()));
+        
+    }
+ 
+    private HorizontalLayout getWeatherListLayout(List<Weather> weatherList) {
+        HorizontalLayout weatherListLayout = new HorizontalLayout();
+        if (weatherList != null) {
+            for (Weather w : weatherList) 
+                weatherListLayout.add(getWeatherLayout(w));
+        }
+
+        return weatherListLayout;
     }
 
-    private void showCoord(Coord coord) {
+    private HorizontalLayout getWeatherLayout(Weather weather) {
+        HorizontalLayout weatherLayout = new HorizontalLayout();
+        weatherLayout.getElement().getStyle().set("border","1px solid black");
+        weatherLayout.setMargin(true);
+        weatherLayout.setSpacing(true);
 
-    }
+        String url = ICON_URL + weather.getIcon() + ".png";
 
-    private void showWeatherList(List<Weather> weatherList) {
-        if (weatherList == null) {
-            return;
-        }
-        String text = "Weather:\n";
+        Image image = new Image(url,url);
+        image.setWidth("100px");
+        image.setHeight("100px");
 
-        for (Weather w : weatherList) {
-            text += "Id: " + String.valueOf(w.getId()) + "\n";
-            text += "Main: " + w.getMain() + "\n";
-            text += "Description: " + w.getDescription() + "\n";
-            text += "Icon: " + w.getIcon() + "\n\n";
-        }
-        add(new TextArea(text));
+        String title = "Weather id: " + weather.getId();
+        String text = "Main: " + weather.getMain() + "\n";
+        text+= "Description: " + weather.getDescription();
+
+        TextArea weatherTextArea = new TextArea();
+        weatherTextArea.setTitle(title);
+        weatherTextArea.setLabel(title);
+        weatherTextArea.setValue(text);
+        weatherTextArea.setWidthFull();
+
+        weatherLayout.add(image, weatherTextArea);
+        return weatherLayout;
     }
 }
